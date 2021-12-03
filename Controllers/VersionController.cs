f592055a7cba30485f153bd5bcfa9eebcb1f7ee7
Tommi Kaura-Aho <tommi.kaura-aho@pinja.com>
2021-12-03 12:43:52 +0200

    Add version controller

---
+++ Controllers/VersionController.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace SampleApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class VersionController : ControllerBase
+    {
+        [HttpGet]
+        public string Get() {
+            return "1.0.0";
+        }
+    }
+}
